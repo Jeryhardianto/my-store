7cc000d289b48353fa361c1dd34d35287b9d7fc2
Jeryhardianto <hardiantojr29@gmail.com>
2024-02-27 23:12:00 +0700

    Add ProductAdminView component

---
+++ src/pages/components/view/admin/Products/index.tsx
@@ -0,0 +1,12 @@
+import AdminLayout from "@/pages/components/layout/AdminLayout";
+
+const ProductAdminView = () => {
+ return (
+  <AdminLayout>
+      <div>
+        <h1>Product Page</h1>
+      </div>
+    </AdminLayout>
+ );
+};
+export default ProductAdminView
